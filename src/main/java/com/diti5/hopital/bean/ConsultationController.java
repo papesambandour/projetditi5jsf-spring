b2e3ab20d533c980c8b5481384a046628e12038e
Pape Samba Ndour <papesambandour@hotmail.com>
2019-07-09 09:25:26 +0000

    add

--- src/main/java/com/diti5/hopital/bean/ConsultationController.java
+++ src/main/java/com/diti5/hopital/bean/ConsultationController.java
@@ -1,9 +1,11 @@
 package com.diti5.hopital.bean;
 
 
-import com.diti5.hopital.dao.RoleDAO;
-import com.diti5.hopital.model.Role;
+import com.diti5.hopital.dao.*;
+import com.diti5.hopital.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -14,40 +16,60 @@ import java.util.List;
 @RequestScoped
 public class ConsultationController {
   @Autowired
-  private RoleDAO roleDAO;
-  private Role role ;
-  private List<Role> roles ; //
+  private ConsultationDAO consultationDAO;
+  @Autowired
+  private PatientDAO patientDAO ;
+  @Autowired
+  private ServiceDOA serviceDOA ;
+  @Autowired
+  private UtilisateurDAO utilisateurDAO ;
+
+  private Consultation consultation;
+  private List<Patient> patients ;
+  private List<Service> services ;
+  private List<Consultation> consultations ;
   @PostConstruct
   public void init(){
-    role = new Role();
-    roles = roleDAO.findAll();
+      consultation = new Consultation();
+      consultation.setUtilisateur(new Utilisateur());
+      consultation.setService(new Service());
+      consultation.setPatient(new Patient());
+      consultations = consultationDAO.findAll();
+      patients = patientDAO.findAll();
+      services = serviceDOA.findAll();
   }
 
   public String save(){
     try {
-      roleDAO.save(role);
-      return "/admin/role/index?faces-redirect=true&succes=true";
+        consultation.setService(serviceDOA.findById(consultation.getService().getId()).get());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object o = auth.getName();
+        String p=o.toString();
+        Utilisateur u = utilisateurDAO.findByUsername(p);
+        consultation.setUtilisateur(u);
+        consultationDAO.save(consultation);
+        return "/admin/consultation/index?faces-redirect=true&succes=true";
     }catch (Exception e)
     {
       System.out.println(e.getMessage());
-      return "/admin/role/index?faces-redirect=true&error=true";
+      return "/admin/consultation/index?faces-redirect=true&error=true";
     }
   }
- public String edit(Role role){
+ public String edit(Consultation consultation){
     try {
-      this.role = role ;
-      return "/admin/role/edit";
+      this.consultation = consultation ;
+      return "/admin/consultation/edit";
     }catch (Exception e)
     {
       System.out.println(e.getMessage());
-      return "/admin/role/index?faces-redirect=true&error=true";
+      return "/admin/consultation/index?faces-redirect=true&error=true";
     }
   }
- public String delete(Role role){
+ public String delete(Consultation consultation){
     try {
-      roleDAO.delete(role);
+      consultationDAO.delete(consultation);
       init();
-      return "/admin/role/index?faces-redirect=tru&success=true";
+      return "/admin/consultation/index?faces-redirect=tru&success=true";
     }catch (Exception e)
     {
       System.out.println(e.getMessage());
@@ -55,5 +77,35 @@ public class ConsultationController {
     }
   }
 
+    public Consultation getConsultation() {
+        return consultation;
+    }
 
+    public void setConsultation(Consultation consultation) {
+        this.consultation = consultation;
+    }
+
+    public List<Patient> getPatients() {
+        return patientDAO.findAll();
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
+
+    public List<Service> getServices() {
+        return serviceDOA.findAll();
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+
+    public List<Consultation> getConsultations() {
+        return consultationDAO.findAll();
+    }
+
+    public void setConsultations(List<Consultation> consultations) {
+        this.consultations = consultations;
+    }
 }
